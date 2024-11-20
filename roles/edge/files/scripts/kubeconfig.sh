bf8bf8b989ed5d3f4de06721bc0af8c145abe606
josandersms <josanders@microsoft.com>
2024-11-20 14:44:44 -0600

    split kubeconfig to new script

---
+++ roles/edge/files/scripts/kubeconfig.sh
@@ -0,0 +1,12 @@
+mkdir ~/.kube
+sudo KUBECONFIG=~/.kube/config:/etc/rancher/k3s/k3s.yaml kubectl config view --flatten > ~/.kube/merged
+mv ~/.kube/merged ~/.kube/config
+chmod  0600 ~/.kube/config
+export KUBECONFIG=~/.kube/config
+
+#switch to k3s context
+kubectl config use-context default
+sudo chmod 644 /etc/rancher/k3s/k3s.yaml
+
+echo "KUBECONFIG=~/.kube/config" >> ~/.bashrc
+source ~/.bashrc
