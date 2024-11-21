9aa137d4a071a30a6b3a7e660a22cdf250073c60
josandersms <josanders@microsoft.com>
2024-11-21 06:01:17 -0600

    more kubeconfig cleanup

--- roles/edge/files/scripts/kubeconfig.sh
+++ roles/edge/files/scripts/kubeconfig.sh
@@ -1,8 +1,9 @@
 mkdir /home/$ADMIN_USERNAME/.kube
 sudo KUBECONFIG=/home/$ADMIN_USERNAME/.kube/config:/etc/rancher/k3s/k3s.yaml kubectl config view --flatten > /home/$ADMIN_USERNAME/.kube/merged
-mv ~/.kube/merged ~/.kube/config
-chmod  0600 ~/.kube/config
-export KUBECONFIG=~/.kube/config
+mv /home/$ADMIN_USERNAME/.kube/merged /home/$ADMIN_USERNAME/.kube/config
+chmod 0600 /home/$ADMIN_USERNAME/.kube/config
+chown -hR $ADMIN_USERNAME:$ADMIN_USERNAME /home/$ADMIN_USERNAME/.kube
+export KUBECONFIG=/home/$ADMIN_USERNAME/.kube/config
 
 #switch to k3s context
 kubectl config use-context default
