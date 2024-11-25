7bc3838bd338f18f9b002f6d3126048cc155a181
josandersms <josanders@microsoft.com>
2024-11-25 09:48:23 -0600

    Add wifi adapter test

--- roles/base/files/scripts/apt-update.sh
+++ roles/base/files/scripts/apt-update.sh
@@ -1,3 +1,4 @@
 sudo apt update
 sudo apt upgrade -y
 sudo apt install linux-modules-extra-`uname -r`
+sudo apt install net-tools
---
+++ roles/nuc/scripts/install-brostrend-wifi-adapter.sh
@@ -0,0 +1,15 @@
+# Just to ensure no issues when running the inistall if dpkg was interrupted at some point prior
+sudo dpkg --configure  -a
+
+# Download and install drivers
+sh -c 'wget linux.brostrend.com/install -O /tmp/install && sh /tmp/install'
+
+# Turn off power save mode to prevent connection instability
+sudo sed 's/^\(wifi.powersave = \).*/wifi.powersave = 2/' -i /etc/NetworkManager/conf.d/default-wifi-powersave-on.conf
+
+# Help with predictable interface names, such as eth0,wlan0 vs wlxa1b2c3d4e5f6 style
+sudo -i
+mkdir -p /etc/default/grub.d
+echo 'GRUB_CMDLINE_LINUX_DEFAULT="splash quiet net.ifnames=0"' >/etc/default/grub.d/ifnames.cfg
+update-grub
+reboot
